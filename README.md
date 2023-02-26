06aadfa3cedacd80b8bcd2b20a87e737489a398c
callmemhz <callmemhz@gmail.com>
2023-02-26 15:40:24 +0800

    use selenium instead of request and beautiful soup, because the website has added a firewall and changed to asynchronously load pages.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Futurama Wallpaper Downloader
+
+A crawler that download Futurama Wallpaper images from
+[Alpha Coders](https://wall.alphacoders.com/by_sub_category.php?id=177585&name=Futurama+Wallpapers)
+
+**Requirement**
+
+- Chrome Driver
+- python 3.9+
+- selenium
+- request
+
+On my MAC
+
+```shell
+# install all the requirements
+$ brew install chromedriver --cask
+$ pip3 install -r requirements.txt
+
+# start to download
+$ python3 main.py
+```
--- main.py
+++ main.py
@@ -1,39 +1,54 @@
-import re
 import os
+import time
 import requests
-from bs4 import BeautifulSoup as BS
-from multiprocessing import Pool
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
 
 
-def get_images_url(page):
-    url = 'https://wall.alphacoders.com/by_sub_category.php?id=177585&name=Futurama+Wallpapers&page=' + str(page)
+def get_images_url():
+    url = 'https://wall.alphacoders.com/by_sub_category.php?id=177585&name=Futurama+Wallpapers'
+    driver.get(url)
+    last_height = driver.execute_script("return document.body.scrollHeight")
 
-    soup = BS(requests.get(url).text, 'lxml')
-    download_buttons = soup.select('.download-button')
-    for button in download_buttons:
-        img_url = button['data-href']
-        yield img_url
+    while True:
+        thumbs = driver.find_elements(By.CLASS_NAME, 'thumb-container')
+        for thumb in thumbs:
+            # img_id = thumb.find_element(By.TAG_NAME, 'a').get_attribute('href').split('/big.php?i=')[1]
+            # prefix = img_id[:3]
+            # img_url = 'https://images.alphacoders.com/{}/{}.png'.format(prefix, img_id)
+            src = thumb.find_element(By.CLASS_NAME, 'img-responsive').get_attribute('src')
+            yield src
 
+        # Scroll down to the bottom.
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-def download_imgae(url):
-    print('Downloading', url)
-    img = requests.get(url)
-    return img.content
+        # Wait to load the page.
+        time.sleep(2)
 
+        # Calculate new scroll height and compare with last scroll height.
+        new_height = driver.execute_script("return document.body.scrollHeight")
 
-def main(i):
-    for url in get_images_url(i):
-        filename = os.getcwd() + '/' + url.split('/')[-4] + '.' + url.split('/')[-2]
-        print(filename)
-        if not os.path.exists(filename):
-            img = download_imgae(url)
-            with open(filename, 'wb') as f:
-                f.write(img)
-                print('OK')
+        if new_height == last_height:
+            break
 
+        last_height = new_height
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'}
 
 if __name__ == '__main__':
-    pool = Pool()
-    pool.map(main, range(1, 9))
-            
+    try:
+        os.mkdir('images')
+    except FileExistsError:
+        pass
 
+    count = 0
+    for url in get_images_url():
+        count += 1
+        print("[{}] download {} ...".format(count, url))
+        filename = 'images/' + url.split('/')[-1]
+        if not os.path.exists(filename):
+            img = requests.get(url, headers=headers).content
+            with open(filename, 'wb') as f:
+                f.write(img)
